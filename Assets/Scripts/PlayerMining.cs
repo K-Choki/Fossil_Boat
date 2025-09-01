05229430b29edbe51ef7256326e903b2aa167714
K-Choki <luckydown_123@naver.com>
2025-09-02 06:11:31 +0900

    Scene Deletee

--- Assets/Scripts/PlayerMining.cs
+++ Assets/Scripts/PlayerMining.cs
@@ -26,16 +26,17 @@ public class PlayerMining : MonoBehaviour
     void OnAttack()
     {
         Debug.Log("좌클릭 입력받음");
-        Ray MiningRay = Camera.main.ViewportPointToRay(MiningInput); //viewport(0.5f,0.5f)
-        RaycastHit[] hits = Physics.RaycastAll(MiningRay, MiningRange, OreLayer);
+        Ray MiningRay = Camera.main.ViewportPointToRay(MiningInput); //화면 가운데로 Ray 쏨쏨쏨쏨쏨
+        RaycastHit[] hits = Physics.RaycastAll(MiningRay, MiningRange, OreLayer); // Mining 방향으로 MiningRange만큼 쏴서 OreLayer만 찾는거임!
 
-        if (hits.Length > 0)
+        if (hits.Length > 0) // 충돌한 옵젝 한개 이상일때! 실행합니당당당!
         {
-            Debug.Log($"RaycastAll 충돌 감지됨: {hits.Length}개 오브젝트");
+            Debug.Log($"RaycastAll 충돌 감지됨: {hits.Length}개 오브젝트"); //몇개 옵젝이 맞았는지 콘솔에 출력하기!
 
-            foreach (RaycastHit hit in hits)
+            foreach (RaycastHit hit in hits) // 모든 옵젝을 검사하기!
             {
                 Debug.Log("충돌한 오브젝트: " + hit.collider.gameObject.name);
+                hit.collider.GetComponent<OreScripts>();
             }
         }
         else
